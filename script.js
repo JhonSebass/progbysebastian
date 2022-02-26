0d4f930e7acccc5f8917835faa3738b4f63176af
= <=>
2022-02-26 14:58:26 -0500

    Actualización perfil

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function(){
+ var texto = "Johann Salazar - Ingeniero de software";
+ maquina("typer",texto,100,0);
+});
+
+function maquina(contenedor,texto,intervalo,n){
+ var i=0;
+  // Creamos el timer
+  timer = setInterval(function() {
+  if ( i<texto.length ) {
+   // Si NO hemos llegado al final del texto..
+   // Vamos añadiendo letra por letra y la _ al final.
+   $("#"+contenedor).html(texto.substr(0,i++) + "_");
+  } else {
+   // En caso contrario..
+   // Salimos del Timer y quitamos la barra baja (_)
+   clearInterval(timer);
+   $("#"+contenedor).html(texto);
+   // Auto invocamos la rutina n veces (0 para infinito)
+   if ( --n!=0 ) {
+    setTimeout(function() {
+     maquina(contenedor,texto,intervalo,n);
+    },3600);
+   }
+  }
+ },intervalo);
+};
